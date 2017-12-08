1150e8e9eb4c2ae5644a73fb26aed0ff4afdf4e8
Michel Grootjans <michel.grootjans@gmail.com>
2017-12-08 22:37:27 +0100

    working implementation

--- src/main/java/XyzService.java
+++ src/main/java/XyzService.java
@@ -1,2 +1,37 @@
+import java.security.MessageDigest;
+import java.text.MessageFormat;
+import java.util.Random;
+
 public class XyzService {
+    public String xyzFn(Target target) {
+        try {
+            String string = String.format("%02d", target.getPublishedOn().getDayOfMonth());
+            string += target.getCategoryPrefix();
+            string += target.getKind().replace("_", "");
+
+            if (target.getHasPersonal()) {
+                string += MessageFormat.format("_{0}", target.getAge()) != null ? target.getAge() : 0;
+            }
+
+            string += "_" + target.getId();
+            MessageDigest md = null;
+            md = MessageDigest.getInstance("SHA1");
+            byte[] result = md.digest(String.valueOf(new Random().nextInt()).getBytes());
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < result.length; i++) {
+                sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
+            }
+
+            string += "_" + sb.toString().substring(0, 8);
+            String truncatedTitle = target.getTitle().toLowerCase().replaceAll("[^A-Za-z]+", "");
+            int length = truncatedTitle.length();
+            int truncateTo = length > 9 ? 9 : length;
+            string += "_" + truncatedTitle.substring(0, truncateTo+1);
+            string += ".jpg";
+            return string;
+        } catch (Exception e) {
+            return "";
+        }
+    }
+
 }
--- src/test/java/XyzServiceTests.java
+++ src/test/java/XyzServiceTests.java
@@ -1,22 +1,16 @@
 import org.junit.Test;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.text.MessageFormat;
 import java.time.LocalDate;
-import java.util.Random;
 
 import static junit.framework.TestCase.assertTrue;
-import static org.hamcrest.CoreMatchers.*;
 
-import static org.hamcrest.MatcherAssert.*;
 import static org.mockito.Mockito.*;
 
 public class XyzServiceTests {
     @Test
     public void name() {
         Target target = mock(Target.class);
-        when(target.getPublishedOn()).thenReturn(LocalDate.of(2017, 12, 14));
+        when(target.getPublishedOn()).thenReturn(LocalDate.of(2017, 12, 8));
         when(target.getCategoryPrefix()).thenReturn("abc");
         when(target.getKind()).thenReturn("unicorn");
         when(target.getHasPersonal()).thenReturn(false);
@@ -24,46 +18,7 @@ public class XyzServiceTests {
         when(target.getId()).thenReturn(1337);
         when(target.getTitle()).thenReturn("magic & superglue");
 
-        String expected = "14abcunicorn_1337_3f4894ca_magicsuper.jpg";
-
-        String result = blah(target);
-        assertThat(result, is(expected));
-        assertTrue(result.split("_")[2].matches("[0-9a-f]{8}"));
-//        assertThat(blah(target), matches("[]"));
-
+        assertTrue(new XyzService().xyzFn(target).matches("08abcunicorn_1337_[0-9a-f]{8}_magicsuper.jpg"));
     }
 
-    private String blah(Target target) {
-        String filename = String.valueOf(target.getPublishedOn().getDayOfMonth());
-        filename += target.getCategoryPrefix();
-        filename += target.getKind().replace("_", "");
-
-        if (target.getHasPersonal()) {
-            filename += MessageFormat.format("_{0}", target.getAge()) != null ? target.getAge() : 0;
-        }
-
-        filename += "_" + target.getId();
-        filename += "_" + sha1().substring(0, 8);
-        String truncatedTitle = target.getTitle().toLowerCase().replaceAll("[^A-Za-z]+", "");
-        int length = truncatedTitle.length();
-        int truncateTo = length > 9 ? 9 : length;
-        filename += "_" + truncatedTitle.substring(0, truncateTo);
-        filename += ".jpg";
-        return filename;
-    }
-
-    private String sha1() {
-        MessageDigest mDigest = null;
-        try {
-            mDigest = MessageDigest.getInstance("SHA1");
-        } catch (Exception e) {
-        }
-        String input = String.valueOf(new Random().nextInt());
-        byte[] result = mDigest.digest(input.getBytes());
-        StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < result.length; i++) {
-            sb.append(Integer.toString((result[i] & 0xff) + 0x100, 16).substring(1));
-        }
-
-        return sb.toString();    }
 }
